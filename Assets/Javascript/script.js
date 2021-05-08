0dddb9f5e7574d5c7a7fb51abf4ce2b49d7a560a
jclark8736 <82688711+jclark8736@users.noreply.github.com>
2021-05-08 19:26:34 -0400

    update with arrays and backup file

--- Assets/Javascript/script.js
+++ Assets/Javascript/script.js
@@ -4,17 +4,19 @@
 
 //special char array
 
-var specialChars = []
+var specialCharArr = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", " < ", "=", " > ", " ? ", "@", "[", "]", " ^ ", "_", "`", "{", "|", "}", "~"]
 
 
 //number array 
-var numbArray = []
+var numberArr = [1,2,3,4,5,6,7,8,9]
 
 //lowercase Array
-var lowerCase = []
+var lowerCaseArr = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
 //Upper Case array
-var upperCase = [].split() // .split will break up an array look up how to do it (from manoli)
+var upperCaseArr = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"] // .split will break up an array look up how to do it (from manoli)
+
+
 //END ARRAY
 
 //function to prompt user for password options
@@ -39,10 +41,65 @@ function getPassword() {
   };
 }
 
+//this stuff all needs to be in the main function
 //create a confirmation prompt  for type of chars
 //need to create conditional statement that checks if user included at least 1 of the char types.return user to start
+var possiblePass = {
+//we need to store length and all characters selected by users
+length:length
+specialChars:specialChars
+}
+return possiblePass;
+
+//create a function for getting a random element from an array
+function getRandomLetters(array) {
+  var random = //array.length randomizer has to have an equation
+  var rand = arr[random]
+  return rand;
+}
 
 
+//create a function to generate a password with the user input
+function generatorPassword() {
+//set a variable to call getPassword
+var options = getPassword();
+
+//variable to store the password as it is being concatenated
+//password will be stored as an array
+var passResult = [];
+
+//create a variable to store the types of chars to include into the password
+var possibleChars = [];
+
+//create a variable to store each type of guaranteed chars
+var guarChars = [];
+
+//write a condition that adds the array of special chars into the array of possible chars 
+//based on user inputs; then push the new random chars into the guaranteed chars
+
+// LOOK UP HOW TO PUSH AND CONCAT REQUIRED FOR THIS ASSIGNMENT
+
+//example
+if (options.specialChars) {
+  possibleChars = possibleChars .amethodwouldgohereconcatenate(specialChars);
+  guarChars.push(getRandomLetters(specialCharsormaybepossibleChars));
+}
+
+
+//create a for loop that iterates over the password length, from the options object
+for (var i = 0; i < options.length; i++) {
+  var possibleCharacter = getRandomLetters(possibleChars):
+  passResult.push(possibleCharacter)
+
+}
+return passResult.join('')
+
+
+}
+
+//then save to a variable
+
+  
 
 
 
